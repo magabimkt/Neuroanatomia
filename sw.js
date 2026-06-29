8006dceda1a7c007d3050f934bc7036a18e79c68
magabimkt <magabi.mkt@gmail.com>
2026-06-29 10:04:15 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,50 +1,71 @@
-const CACHE = 'neuro-sem-neura-v0.3.2';
-const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png',
-  './assets/mapas/01_neurulacao.jpg',
-  './assets/mapas/02_potencial_acao.jpg',
-  './assets/mapas/03_celulas_gliais.jpg',
-  './assets/mapas/04_lobos_cerebro.jpg',
-  './assets/mapas/05_nucleos_base.jpg',
-  './assets/mapas/06_tratos_medula.jpg',
-  './assets/mapas/07_sna.jpg',
-  './assets/mapas/08_nervos_cranianos_p1.jpg',
-  './assets/mapas/09_nervos_cranianos_p2.jpg',
-  './assets/mapas/10_circuitos_dopamina.jpg',
-  './assets/mapas/11_tronco_encefalico.jpg',
-  './assets/mapas/12_esquizofrenia.jpg',
-  './assets/mapas/organizacao-sistema-nervoso.svg',
-  './assets/mapas/medula-espinhal.svg',
-  './assets/mapas/tronco-encefalico.svg',
-  './assets/mapas/cerebelo.svg',
-  './assets/mapas/diencefalo.svg',
-  './assets/mapas/telencefalo.svg',
-  './assets/mapas/sistema-limbico.svg',
-  './assets/mapas/nervos-cranianos.svg',
-  'https://fonts.googleapis.com/css2?family=Fraunces:ital,opsz,wght@0,9..144,300;0,9..144,600;0,9..144,700;1,9..144,300&family=Atkinson+Hyperlegible:wght@400;700&family=IBM+Plex+Mono:wght@400;500&display=swap'
+const CACHE_NAME = "neuro-sem-neura-v1-2";
+
+const CORE_ASSETS = [
+  "./",
+  "./index.html",
+  "./quiz.html",
+  "./jogos.html",
+  "./manifest.json",
+  "./offline.html",
+  "./assets/icons/icon-192.png",
+  "./assets/icons/icon-512.png",
+  "./assets/icons/maskable-512.png",
+  "./assets/mapas/01_neurulacao.png",
+  "./assets/mapas/02_potencial_acao.png",
+  "./assets/mapas/03_celulas_gliais.png",
+  "./assets/mapas/04_lobos_cerebro.png",
+  "./assets/mapas/05_nucleos_base.png",
+  "./assets/mapas/06_tratos_medula.png",
+  "./assets/mapas/07_sna.png",
+  "./assets/mapas/08_nervos_cranianos_p1.png",
+  "./assets/mapas/09_nervos_cranianos_p2.png",
+  "./assets/mapas/10_circuitos_dopamina.png",
+  "./assets/mapas/11_tronco_encefalico.png",
+  "./assets/mapas/12_esquizofrenia.png",
+  "./assets/mapas/13_cerebelo.png",
+  "./assets/mapas/14_diencefalo.png",
+  "./assets/mapas/15_sistema_limbico.png"
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(CORE_ASSETS))
+      .then(() => self.skipWaiting())
+  );
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
 });
 
-self.addEventListener('fetch', e => {
-  // Don't intercept cross-origin iframe requests
-  if (new URL(e.request.url).origin !== location.origin) {
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+          return response;
+        })
+        .catch(() => caches.match(event.request).then(cached => cached || caches.match("./offline.html")))
+    );
     return;
   }
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
-      const clone = res.clone();
-      caches.open(CACHE).then(c => c.put(e.request, clone));
-      return res;
-    }))
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return cached || fetch(event.request).then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+        return response;
+      });
+    })
   );
 });
